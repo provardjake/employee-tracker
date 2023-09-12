f4cb847ff684e6e8ef3c2fff3dfac42395d9f24b
Jake Provard <provardjake@gmail.com>
2023-09-11 20:26:47 -0600

    added js functions to add new department and new role

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require('dotenv').config()
 
+// creates the connection to the sql database
 const db = mysql.createConnection({
     host: "localhost",
     user: process.env.DB_USER,
@@ -9,6 +10,7 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+// function to prompt the user for what they would like to do
 function init(){
     console.log("Main Menu");
 
@@ -60,24 +62,91 @@ function init(){
     })
 }
 
+// views all the department table in the sql database
 function viewDepartments(){
     db.query("SELECT * FROM department;", (err, res) =>{
         err ? console.log(err) : console.table(res), init();
     })
 }
 
+// views the roles table in the sql database
 function viewRoles(){
     db.query("SELECT * FROM roles;", (err, res) =>{
         err ? console.log(err) : console.table(res), init();
     })
 }
 
+// views the employee table in the sql database
 function viewEmployees(){
     db.query("SELECT * FROM employee;", (err, res) =>{
         err ? console.log(err) : console.table(res), init();
     })
 }
 
+// adds a new department to the department table from the user entered parameter
+function addDepartment(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the name of the department: ",
+            name: "newDepartment"
+        }
+    ])
+    .then((res) =>{
+        db.query(`INSERT INTO department (name) VALUES ("${res.newDepartment}");`, (err, res) =>{
+            err ? console.log(err) : console.log("New department added!"), viewDepartments();
+        });
+    });
+}
 
+// adds a new role to the roles table from user entered parameters
+function addRole(){
+    const departmentId = [];
+    const departmentName = [];
+    // creates the list of departments from the existing table
+    db.query("SELECT * FROM department;", (err, res) =>{
+        if (err){
+            console.log(err);
+        }
+        else{
+            for(let i = 0; i < res.length; i++){
+                departmentId.push(res[i].id);
+                departmentName.push(res[i].name);
+            }
+        }
+    })
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the title of the new role: ",
+            name: "newRole"
+        },
+        {
+            type: "input",
+            message: "Enter the salary of the new role: ",
+            name: "newSalary"
+        },
+        {
+            type: "list",
+            message: "Select the department the new role belongs to",
+            name: "newDepartment",
+            choices: departmentName
+        }
+    ])
+    .then((res) =>{
+        let newDepartmentId;
+        // assigns the id of the department to the corresponding user selection
+        for(let x = 0; x < departmentId.length; x++){
+            if(res.newDepartment === departmentName[x]){
+                newDepartmentId = departmentId[x];
+                break;
+            }
+        }
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${res.newRole}", ${res.newSalary}, ${newDepartmentId});`, (err, res) =>{
+            err ? console.log(err) : console.log("New Role Added!"), viewRoles();
+        });
+    });
+}
 
 init();
